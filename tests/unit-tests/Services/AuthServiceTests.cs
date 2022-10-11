2bc9f097d55e7989a5b40689305030c8abe20490
Maija Pietilä <pietilamai@gmail.com>
2022-10-11 19:03:14 +0300

    Initialize AuthServiceTests class.

---
+++ tests/unit-tests/Services/AuthServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using shopping_bag.Config;
+using shopping_bag.Services;
+
+namespace shopping_bag_unit_tests
+{
+    public class AuthServiceTests
+    {
+        private AuthService _authServiceMock;
+        private readonly Mock<AppDbContext> _appDbContextMock = new Mock<AppDbContext>();
+        private readonly Mock<IUserService> _iUserServiceMock = new Mock<IUserService>();
+        public AuthServiceTests()
+        {
+            _authServiceMock = new AuthService(_appDbContextMock.Object, _iUserServiceMock.Object);
+        }
+
+    }
+}
