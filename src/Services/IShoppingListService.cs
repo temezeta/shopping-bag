e829365c9b0e77fa0c58bb36a8799f5276c1421b
Santeri <santeri.kangas@tuni.fi>
2022-10-21 13:02:15 +0300

    Implement endpoint for removing shopping lists (#47)

--- src/Services/IShoppingListService.cs
+++ src/Services/IShoppingListService.cs
@@ -8,6 +8,7 @@ namespace shopping_bag.Services
     {
         Task<ServiceResponse<ShoppingList>> AddShoppingList(AddShoppingListDto shoppingList);
         Task<ServiceResponse<ShoppingList>> ModifyShoppingList(ModifyShoppingListDto shoppingListData, long shoppingListId);
+        Task<ServiceResponse<bool>> RemoveShoppingList(long shoppingListId);
         Task<ServiceResponse<Item>> AddItemToShoppingList(AddItemDto itemToAdd);
         Task<ServiceResponse<bool>> RemoveItemFromShoppingList(User user, long itemId);
         Task<ServiceResponse<Item>> ModifyItem(User user, ModifyItemDto itemToModify, long itemId);
