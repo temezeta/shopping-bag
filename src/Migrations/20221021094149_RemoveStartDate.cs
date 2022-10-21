63b4b5e73fcf7f22319c8cb17b81893b73fccfae
Santeri Kangas <santeri.kangas@tuni.fi>
2022-10-21 12:42:47 +0300

    Add migrations for removing start date

---
+++ src/Migrations/20221021094149_RemoveStartDate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopping_bag.Migrations
+{
+    public partial class RemoveStartDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "ShoppingLists");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "ShoppingLists",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
