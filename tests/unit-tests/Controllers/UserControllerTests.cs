a417ee332880ed5f06d3deacc02d6bec49172f9e
Teemu Heino <teemu_heino@outlook.com>
2022-11-14 10:49:51 +0200

    Add unit tests

---
+++ tests/unit-tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using shopping_bag.Controllers;
+using shopping_bag.Models.User;
+using shopping_bag.Models;
+using shopping_bag.Services;
+using shopping_bag.DTOs.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shopping_bag_unit_tests.Controllers
+{
+    public class UserControllerTests
+    {
+        private readonly UserController _sut;
+        private readonly Mock<IUserService> _userService = new Mock<IUserService>();
+
+        public UserControllerTests()
+        {
+            _userService.Setup(x => x.GetUserByEmail(It.IsAny<string>())).ReturnsAsync(new ServiceResponse<User>(new User()));
+            _sut = new UserController(_userService.Object, UnitTestHelper.GetMapper())
+            {
+                ControllerContext = UnitTestHelper.GetLoggedInControllerContext()
+            };
+        }
+
+        #region Change Password
+        [Fact]
+        public async Task ChangePassword_UserNotFound_ReturnsBadRequest()
+        {
+            _userService.Setup(x => x.GetUserByEmail(It.IsAny<string>())).ReturnsAsync(new ServiceResponse<User>(error: "User not found"));
+
+            var res = await _sut.ChangePassword(new ChangePasswordDto
+            {
+                CurrentPassword = "string1A?",
+                NewPassword = "string1B?",
+                RepeatNewPassword = "string1B?"
+            });
+
+            Assert.IsType<BadRequestResult>(res);
+        }
+
+        [Fact]
+        public async Task ChangePassword_ChangeFailed_ReturnsBadRequest()
+        {
+            _userService.Setup(it => it.ChangeUserPassword(It.IsAny<long>(), It.IsAny<ChangePasswordDto>()))
+                .ReturnsAsync(new ServiceResponse<User>(error: "Change failed"));
+
+            var res = await _sut.ChangePassword(new ChangePasswordDto
+            {
+                CurrentPassword = "string1A?",
+                NewPassword = "string1B?",
+                RepeatNewPassword = "string1B?"
+            });
+
+            Assert.IsType<BadRequestResult>(res);
+        }
+
+        [Fact]
+        public async Task ChangePassword_ChangeSuccess_ReturnsUser()
+        {
+            _userService.Setup(it => it.ChangeUserPassword(It.IsAny<long>(), It.IsAny<ChangePasswordDto>()))
+                .ReturnsAsync(new ServiceResponse<User>(new User()));
+
+            var res = await _sut.ChangePassword(new ChangePasswordDto
+            {
+                CurrentPassword = "string1A?",
+                NewPassword = "string1B?",
+                RepeatNewPassword = "string1B?"
+            });
+
+            Assert.NotNull(res);
+            var okResult = res as OkObjectResult;
+            Assert.NotNull(okResult);
+            Assert.IsType<UserDto>(okResult.Value);
+        }
+        #endregion
+    }
+}
---
+++ tests/unit-tests/Services/BaseServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shopping_bag.Config;
+using shopping_bag.Models.User;
+using shopping_bag.Models;
+
+namespace shopping_bag_unit_tests.Services
+{
+    public class BaseServiceTest
+    {
+        private readonly List<Office> Offices;
+        private readonly List<UserRole> UserRoles;
+        private readonly List<User> Users;
+
+        public BaseServiceTest()
+        {
+            Offices = new List<Office>()
+            {
+                new Office() { Id = 1, Name = "Tampere" }
+            };
+            UserRoles = new List<UserRole>()
+            {
+                new UserRole() { RoleId = 1, RoleName = "User" },
+                new UserRole() { RoleId = 2, RoleName = "Admin" }
+            };
+            Users = new List<User>()
+            {
+                new User() { Id = 1, UserRoles = new List<UserRole>() { UserRoles[0] }, Email = "", FirstName = "Normal", LastName = "User", PasswordHash = Array.Empty<byte>(), PasswordSalt = Array.Empty<byte>(), HomeOffice = Offices[0] },
+                new User() { Id = 2, UserRoles = new List<UserRole>() { UserRoles[1] }, Email = "", FirstName = "Admin", LastName = "User", PasswordHash = Array.Empty<byte>(), PasswordSalt = Array.Empty<byte>(), HomeOffice = Offices[0] }
+            };
+        }
+
+        protected AppDbContext GetDatabase()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+            SetupDb(context);
+
+            return context;
+        }
+
+        private void SetupDb(AppDbContext context)
+        {
+            context.RemoveRange(context.ShoppingLists.ToList());
+            context.RemoveRange(context.Items.ToList());
+            context.RemoveRange(context.Offices.ToList());
+            context.RemoveRange(context.Users.ToList());
+            context.RemoveRange(context.UserRoles.ToList());
+            context.SaveChanges();
+
+            context.Offices.AddRange(Offices);
+            context.SaveChanges();
+            context.UserRoles.AddRange(UserRoles);
+            context.SaveChanges();
+            context.Users.AddRange(Users);
+            context.SaveChanges();
+        }
+    }
+}
