77bf87008bc775926d1c0c2d1026957fd37d1047
Santeri <santeri.kangas@tuni.fi>
2022-12-01 14:15:42 +0200

    Remove preseeded shopping lists (#115)
    Also, add ReminderSettings to DefaultAdmin

--- src/Config/ModelBuilderExtensions.cs
+++ src/Config/ModelBuilderExtensions.cs
@@ -60,84 +60,10 @@ namespace shopping_bag.Config
             }
         };
 
-        private static readonly ShoppingList[] ShoppingLists = new ShoppingList[]
-        {
-            new ShoppingList()
-            {
-                Id = 1,
-                Name = "Week 50 list",
-                Comment = "Weekly order",
-                Ordered = false,
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                DueDate = new DateTime(2022, 12, 18, 17, 00, 00),
-                ExpectedDeliveryDate = new DateTime(2022, 12, 20, 17, 00, 00),
-                OfficeId = 1,
-                UserId = null
-            },
-            new ShoppingList()
-            {
-                Id = 2,
-                Name = "Office supplies",
-                Comment = "List for office supplies",
-                Ordered = false,
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                DueDate = new DateTime(2023, 1, 15, 22, 00, 00),
-                ExpectedDeliveryDate = new DateTime(2023, 2, 15, 12, 00, 00),
-                OfficeId = 1,
-                UserId = null
-    },
-            new ShoppingList()
-            {
-                Id = 3,
-                Name = "Tampere office list",
-                Comment = "No due or delivery dates set",
-                Ordered = false,
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                OfficeId = 5,
-                UserId = null
-            },
-            new ShoppingList()
-            {
-                Id = 4,
-                Name = "Week 40 list",
-                Comment = "Order that is overdue but not delivered",
-                Ordered = true,
-                OrderedDate = new DateTime(2022, 10, 10, 16, 00, 00),
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                DueDate = new DateTime(2022, 10, 9, 17, 00, 00),
-                ExpectedDeliveryDate = new DateTime(2023, 1, 15, 12, 00, 00),
-                OfficeId = 1,
-                UserId = null
-            },
-            new ShoppingList()
-            {
-                Id = 5,
-                Name = "Week 39 list",
-                Comment = "Order that is overdue and delivered",
-                Ordered = true,
-                OrderedDate = new DateTime(2022, 10, 10, 16, 00, 00),
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                DueDate = new DateTime(2022, 9, 30, 17, 00, 00),
-                ExpectedDeliveryDate = new DateTime(2022, 10, 3, 12, 00, 00),
-                OfficeId = 1,
-                UserId = null
-            },
-            new ShoppingList()
-            {
-                Id = 6,
-                Name = "List with only a name",
-                Ordered = false,
-                CreatedDate = new DateTime(2022, 10, 12, 12, 00, 00),
-                OfficeId = 1,
-                UserId = null
-            }
-        };
-
         public static void Seed(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Office>().HasData(Offices);
             modelBuilder.Entity<UserRole>().HasData(UserRoles);
-            modelBuilder.Entity<ShoppingList>().HasData(ShoppingLists);
         }
 
         public static void SeedDefaultAdmin(this AppDbContext context)
@@ -179,6 +105,17 @@ namespace shopping_bag.Config
                 VerificationToken = verificationToken,
                 VerifiedAt = DateTime.Now, // Don't require verification for default user
             };
+            var reminderSettings = new ReminderSettings()
+            {
+                ReminderDaysBeforeDueDate = new List<int>() { 2 },
+                ReminderDaysBeforeExpectedDate = new List<int>() { 2 },
+                DueDateRemindersDisabled = false,
+                ExpectedRemindersDisabled = true,
+                UserId = user.Id,
+                AllRemindersDisabled = false
+            };
+            user.ReminderSettings = reminderSettings;
+
             user.UserRoles.AddRange(roles);
             context.Users.Add(user);
             context.SaveChanges();
